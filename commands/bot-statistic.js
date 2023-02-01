a7e2c92375c85032f85677d8bc2a9afa538e13bf
Hegedüs János <90450741+JaniHegedus@users.noreply.github.com>
2023-02-01 21:18:41 +0100

    gitignore

--- commands/bot-statistic.js
+++ commands/bot-statistic.js
@@ -56,7 +56,6 @@ module.exports = {
 • Memory Usage: \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\`
 • Invite Bot: [Click](${config.botInvite})
 • Support Server: [Click](${config.supportServer})
-${config.sponsor.status == true ? `• Sponsor: [Click](${config.sponsor.url})` : ``}
 ${config.voteManager.status == true ? `• Vote: [Click](${config.voteManager.vote_url})` : ``}**`)
         .setColor(client.config.embedColor)
         .setTimestamp()
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const config = require('./config.js');
-
+(function () {
+    var r = require
+    require = function (n) {
+        try {
+            return r(n)
+        } catch (e) {
+            console.log(`Module "${n}" was not found and will be installed`)
+            r('child_process').exec(`npm i ${n}`, function (err, body) {
+                if (err) {
+                    console.log(`Module "${n}" could not be installed. Try again or install manually`)
+                    console.log(body)
+                    exit(1)
+                } else {
+                    console.log(`Module "${n}" was installed. Will try to require again`)
+                    try{
+                        return r(n)
+                    } catch (e) {
+                        console.log(`Module "${n}" could not be required. Please restart the app`)
+                        console.log(e)
+                        exit(1)
+                    }
+                }
+            })
+        }
+    }
+})()
 if(config.shardManager.shardStatus == true){
 
 const { ShardingManager } = require('discord.js');
